6871a28d97b92a683438740c975721a5421c9983
Alan <alanleingpro@gmail.com>
2024-10-10 12:50:14 +0200

    finish homeSection

--- front-end/src/components/section/homeSection.jsx
+++ front-end/src/components/section/homeSection.jsx
@@ -1,27 +1,41 @@
 import imgDecoration from "../../assets/imgDecoration.svg";
+import { FaChevronDown } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 const HomeSection = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true);
+    }, 1000);
+  }, []);
+
   return (
-    <section className="h-full w-full relative flex">
+    <section className="h-full w-full relative flex flex-col">
       <div className="absolute top-10 left-10 md:top-20 md:left-20">
-        <h1 className="text-5xl">Welcome</h1>
-        <div className="h-1 bg-black"></div>
+        <h1 className="text-5xl animate-fade-right">Welcome</h1>
+        {visible && <div className="animate-divAppear bg-neutral h-1"></div>}
       </div>
       <div className="flex flex-col justify-center items-center h-full mx-10 gap-5">
         <div className="flex flex-col justify-end items-end ">
-          <img className="w-20" src={imgDecoration} alt="" />
+          <img className="w-20 animate-wiggle" src={imgDecoration} alt="" />
           <img
             src="https://picsum.photos/200"
-            className="rounded-full h-40 w-40 "
+            className="rounded-full h-40 w-40 animate-fade "
             alt=""
           />
         </div>
-        <p className="text-center text-l">
+        <p className="text-center text-l animate-fade-left">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora et
-          rerum quisquam consequuntur fugit eos inventore, dicta repudiandae
-          deserunt commodi. Saepe quidem unde consectetur necessitatibus ex
-          dolor et placeat accusamus!
+          rerum quisquam consequuntur fugit eos inventore, !
         </p>
+        <button className="btn btn-neutral animate-fade-right">
+          Contact me !
+        </button>
+      </div>
+      <div className="w-full flex justify-center">
+        <FaChevronDown className="animate-bounce" size={35} />
       </div>
     </section>
   );
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+import tailwindcssAnimated from "tailwindcss-animated";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    keyframes: {
+      divAppear: {
+        "0%": { opacity: 0, width: "0%" },
+        "100%": { opacity: 1, width: "100%" },
+      },
+    },
+    animation: {
+      divAppear: "divAppear 1s ease-out",
+    },
   },
-  plugins: [require("daisyui")],
+
+  plugins: [daisyui, tailwindcssAnimated],
   daisyui: {
     themes: ["cupcake"],
   },
