29bc842e66ab1ff6a7f8eb30ad0cab5879f7a28f
Alan <alanleingpro@gmail.com>
2024-10-15 14:18:34 +0200

    Refactor footer links to open in new tab

--- front-end/src/components/footer.jsx
+++ front-end/src/components/footer.jsx
@@ -22,13 +22,21 @@ const Footer = () => {
           <p className="text-xl">© {year} Alan Leing</p>
         </div>
         <div className="flex gap-5">
-          <a href="www.linkedin.com/in/alan-leing-869a44273" rel="noreferer">
+          <a
+            href="www.linkedin.com/in/alan-leing-869a44273"
+            rel="noreferer"
+            target="_blank"
+          >
             <FaLinkedin className="text-3xl text-neutral hover:text-accent transition ease-in-out 0.5s" />
           </a>
-          <a href="https://github.com/CodeWithAlan-js" rel="noreferer">
+          <a
+            href="https://github.com/CodeWithAlan-js"
+            rel="noreferer"
+            target="_blank"
+          >
             <FaGithub className="text-3xl text-neutral hover:text-accent  transition ease-in-out 0.5s" />
           </a>
-          <a href="https://x.com/CodeWithAlan" rel="noreferer">
+          <a href="https://x.com/CodeWithAlan" rel="noreferer" target="_blank">
             <FaXTwitter className="text-3xl text-neutral hover:text-accent  transition ease-in-out 0.5s" />
           </a>
         </div>
